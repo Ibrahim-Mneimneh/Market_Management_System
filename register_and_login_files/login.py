e5ee5e1cfc4c815c28dbcd5581015ed12857a1d3
Zouheir Nakouzi <zouheir2002@gmail.com>
2023-04-17 15:46:17 +0300

    dark mode for login

--- register_and_login_files/login.py
+++ register_and_login_files/login.py
@@ -1,14 +1,17 @@
-import tkinter as tk
 import hashlib
 import subprocess
+import customtkinter
 
-root = tk.Tk()
+customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+root = customtkinter.CTk()
 root.title("Login")
 # root.geometry("220x280")
 
 # Centering the window on the screen
-window_width = 220
-window_height = 280
+window_width = 230
+window_height = 310
 x = int(int(root.winfo_screenwidth() / 2) - int(window_width / 2))
 y = int(int(root.winfo_screenheight() / 2) - int(window_height / 2))
 root.geometry(f"{window_width}x{window_height}+{x}+{y}")
@@ -18,14 +21,14 @@ root.resizable(width=False, height=False)
 
 
 def authenticate():
-    def message(message="Please fill all the fields!", color='red'):
+    def message(m="Please fill all the fields!", color='red'):
         # Destroy previous message label
         widgets = root.grid_slaves(row=4, column=0)
         for widget in widgets:
             widget.destroy()
 
         # Create a label widget if the fields are not all filled
-        tk.Label(root, text=message, fg=color).grid(row=4, column=0, columnspan=3)
+        customtkinter.CTkLabel(master=root, text=m, text_color=color).grid(row=4, column=0, columnspan=3)
 
     # Check if all entry fields are filled
     if usernamevar.get() and passwordvar.get():
@@ -39,17 +42,17 @@ def authenticate():
         # TODO Get username from MySQL and compare it to usernamevar.get()
         sqlusername = ""  # Store it here
         if not usernamevar.get() == sqlusername:
-            message("Username or password incorrect.")
+            message("Incorrect username or password.")
             return
 
         # TODO Get sha256 pass from MySQL and compare it to passwordvar.get()
         sqlpassword = ""  # Store it here
         if not passwordvar.get() == sqlpassword:
-            message("Username or password incorrect.")
+            message("Incorrect username or password.")
             return
 
         # Create a label widget to display registration success message
-        message(message="Login Successful!", color='green')
+        message(m="Login Successful!", color='green')
     else:
         message()
 
@@ -62,13 +65,13 @@ def registerpopup():
 
 
 # Heading
-tk.Label(root, text="Login", font="Arial 15 bold").grid(row=0, column=0, columnspan=10, pady=10)
-tk.Label(root, text="").grid(row=4, column=0, columnspan=3)
+customtkinter.CTkLabel(root, text="Login", font=("Arial", 15, "bold")).grid(row=0, column=0, columnspan=10, pady=10)
+customtkinter.CTkLabel(root, text="").grid(row=4, column=0, columnspan=3)
 
 # Field Name
-username = tk.Label(root, text="Username")
-password = tk.Label(root, text="Password")
-register = tk.Label(root, text="Don't have an account? Register now.")
+username = customtkinter.CTkLabel(root, text="Username")
+password = customtkinter.CTkLabel(root, text="Password")
+register = customtkinter.CTkLabel(root, text="Don't have an account? Register now.")
 
 # Packing Fields
 username.grid(row=1, column=0, padx=10, pady=10, sticky="w")
@@ -76,21 +79,21 @@ password.grid(row=2, column=0, padx=10, pady=10, sticky="w")
 register.grid(row=5, column=0, columnspan=10, padx=10, pady=10, sticky="w")
 
 # Variables for storing data
-usernamevar = tk.StringVar()
-passwordvar = tk.StringVar()
+usernamevar = customtkinter.StringVar()
+passwordvar = customtkinter.StringVar()
 
 # Creating entry field
-usernameentry = tk.Entry(root, textvariable=usernamevar)
-passwordentry = tk.Entry(root, show='*', textvariable=passwordvar)
+usernameentry = customtkinter.CTkEntry(root, textvariable=usernamevar)
+passwordentry = customtkinter.CTkEntry(root, show='*', textvariable=passwordvar)
 
 # Packing entry field
 usernameentry.grid(row=1, column=2, pady=10, sticky="e")
 passwordentry.grid(row=2, column=2, pady=10, sticky="e")
 
 # Login button
-tk.Button(text="Login", command=authenticate).grid(row=3, column=0, columnspan=10, pady=10)
+customtkinter.CTkButton(master=root,text="Login", command=authenticate).grid(row=3, column=0, columnspan=10, pady=10)
 
 # Register button
-tk.Button(text="Register", command=registerpopup).grid(row=6, column=0, columnspan=10)
+customtkinter.CTkButton(master=root,text="Register", command=registerpopup).grid(row=6, column=0, columnspan=10)
 
 root.mainloop()
