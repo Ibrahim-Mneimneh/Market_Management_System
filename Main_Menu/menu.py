f6a8f826ecc949c83bbaa78ef05916f3d5334829
Zouheir Nakouzi <zouheir2002@gmail.com>
2023-04-22 18:30:06 +0300

    added config.cfg commands

--- Main_Menu/menu.py
+++ Main_Menu/menu.py
@@ -1,21 +1,53 @@
+import configparser
+import os
+import subprocess
 import eel
 import mysql.connector
 import hashlib
 
+file_path = "../config.cfg"
+
+if not os.path.exists(file_path):
+    subprocess.run(['python', '../SQL/getSQLpassword.py'])
+
+config = configparser.ConfigParser()
+config.read(file_path)
+try:
+    mydb = mysql.connector.connect(
+        host=config.get('mysql', 'host'),
+        user=config.get('mysql', 'user'),
+        password=config.get('mysql', 'password'),
+        port=3306,
+        database=config.get('mysql', 'database')
+    )
+except mysql.connector.Error as error:
+    print("Database Connection Failed!")
+    quit()
+    mydb.close()
+
 
 @eel.expose
-def signup(sqlpassword, firstname, lastname, email, username, phonenumber, password):
-    try:
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password=sqlpassword,
-            port=3306,
-            database="mms")
-    except mysql.connector.Error as error:
-        print("Database Connection Failed!")
-        quit()
-        mydb.close()
+def signup(firstname, lastname, email, username, phonenumber, password):
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT EXISTS(SELECT * FROM Account WHERE username = %s)", (username,))
+    result = mycursor.fetchone()[0]
+
+    if result == 1:
+        return "Username already exists."
+
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT EXISTS(SELECT * FROM Account WHERE email = %s)", (email,))
+    result = mycursor.fetchone()[0]
+
+    if result == 1:
+        return "Email already exists."
+
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT EXISTS(SELECT * FROM Employee WHERE PhoneNumber = %s)", (phonenumber,))
+    result = mycursor.fetchone()[0]
+
+    if result == 1:
+        return "Phone number already exists."
 
     if not len(password) >= 8:
         return "Password must be at least 8 characters long."
@@ -49,23 +81,11 @@ def signup(sqlpassword, firstname, lastname, email, username, phonenumber, passw
     except mysql.connector.IntegrityError as error:
         print("Couldn't insert the record to the database, an integrity constraint failed!")
         return "Username or email may be already in use."
-    return"Signed up Successfully"
+    return "Signed up Successfully"
 
 
 @eel.expose
-def login(sqlPassword, username_email, password):
-    try:
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password=sqlPassword,
-            port=3306,
-            database="mms")
-    except mysql.connector.Error as error:
-        print("Database Connection Failed!")
-        quit()
-        mydb.close()
-
+def login(username_email, password):
     if ".com" in username_email:
         try:
             query = "SELECT username from Account where email=\"" + username_email + "\""
